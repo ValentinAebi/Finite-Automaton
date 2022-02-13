a8a52141044fc13a194feab87a1b4f5416d8f7f0
Valentin Aebi <valentin.aebi@epfl.ch>
2022-02-13 16:36:43 +0100

    Mark if accepting + minor layout changes

--- src/main/kotlin/com/github/valentinaebi/nfasim/gui/GuiState.kt
+++ src/main/kotlin/com/github/valentinaebi/nfasim/gui/GuiState.kt
@@ -1,16 +1,18 @@
 package com.github.valentinaebi.nfasim.gui
 
-import javafx.beans.binding.Bindings
+import com.github.valentinaebi.nfasim.main
 import javafx.scene.Group
 import javafx.scene.control.Label
 import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
 import javafx.scene.shape.Polygon
+import javafx.scene.shape.StrokeType
 import javafx.scene.text.Font
 import com.github.valentinaebi.nfasim.automaton.FiniteAutomaton.Companion.State as State
 
-class GuiState(val underlyingState: State): Group() {
-    private val circle = Circle(radius, colorInactive)
+class GuiState(val underlyingState: State, val owner: AutomatonPane): Group() {
+    private val mainCircle = Circle(radius, colorInactive)
+    private val acceptMarkerCircle = Circle(radius + strokeWidth, Color.TRANSPARENT)
     private val shadow = Circle(shadowRadius, colorShadow)
     private val nameLabel = Label(underlyingState.id)
     private val startTriangle = Polygon()
@@ -23,42 +25,55 @@ class GuiState(val underlyingState: State): Group() {
     var isActive = false
         set(_isActive){
             field = _isActive
-            circle.fill = if (isActive) colorActive else colorInactive
+            mainCircle.fill = if (isActive) colorActive else colorInactive
         }
     var isInit = false
         set(_isInit){
             field = _isInit
             startTriangle.isVisible = isInit
+            owner.setInitState(this)
         }
     var isAccepting = false
         set(_isAccepting){
             field = _isAccepting
-            TODO("mark a state as accepting or not")
+            acceptMarkerCircle.isVisible = isAccepting
         }
 
     init {
+        mainCircle.strokeWidth = strokeWidth
+        mainCircle.stroke = colorStroke
+        mainCircle.strokeType = StrokeType.INSIDE
+        acceptMarkerCircle.strokeWidth = strokeWidth
+        acceptMarkerCircle.stroke = colorStroke
+        acceptMarkerCircle.strokeType = StrokeType.OUTSIDE
+        acceptMarkerCircle.isVisible = false
         nameLabel.font = font
         shadow.isVisible = false
-        startTriangle.points.setAll(listOf(-30.0 - radius, 10.0, -30.0 - radius, -10.0, -radius, 0.0))
+        startTriangle.points.setAll(listOf(
+            -triangleLength - radius, triangleHeight / 2.0,
+            -triangleLength - radius, -triangleHeight / 2.0,
+            -radius, 0.0
+        ))
         startTriangle.fill = colorTriangle
         startTriangle.isVisible = false
-        children.addAll(shadow, circle, nameLabel, startTriangle)
+        children.addAll(shadow, acceptMarkerCircle, mainCircle, nameLabel, startTriangle)
         nameLabel.layoutX = -nameLabel.boundsInParent.width / 2.0
         nameLabel.layoutY = -12.5
-        setOnMouseClicked { event -> if (event.isStillSincePress) isSelected = !isSelected }
         setOnMouseDragged { event -> layoutX = event.sceneX ; layoutY = event.sceneY }
     }
 
     companion object {
         const val radius = 35.0
         private const val shadowRadius = 45.0
-        private const val triangleLength = 25.0
-        private const val triangleHeight = 10.0
+        private const val triangleLength = 30.0
+        private const val triangleHeight = 20.0
+        private const val strokeWidth = 3.0
         private val font = Font("cambria", 24.0)
         private val colorInactive = Color.YELLOW
         private val colorActive = Color.ORANGE
         private val colorShadow = Color.LIGHTGRAY
         private val colorTriangle = Color.GREEN
+        private val colorStroke = Color.BLACK
     }
 
 }
